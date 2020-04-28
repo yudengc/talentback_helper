6973290e078c6a570e45acc34a115866b8178440
dengyu <yudengc@outlook.com>
2020-04-28 18:46:31 +0800

    1

--- talentback_helper.py
+++ talentback_helper.py
@@ -7,46 +7,77 @@ import sys
 import tkinter as tk
 
 
+FUNC_TEMPLATE = '''
+def go_{button}():
+
+'''
 
 class Talentback:
     def __init__(self):
         self.window = tk.Tk()
+        self.window.title("达人推客服助手")
         self.token = None
-        self.window.geometry('600x400')
-        self.window.resizable(width=False, height=False)
-        self.base_url = "http://api.etest.darentui.com/"
-        self.login_face()
-        self.window.title = "达人推后台助手"
-        self.window.mainloop()       
+        self.width = 400
+        self.heigth = 200
+        self.window.geometry('{}x{}'.format(self.width, self.heigth))
+        # self.window.resizable(width=False, height=False)
+        self.window.minsize(400, 200)
+        self.window.maxsize(400, 600)
+        self.base_url = "http://api.etest.darentui.com/"        
+        self.menu()
+        # self.login_face()
+        self.main_face()
+        self.window.mainloop()
+
+    def menu(self):
+        menubar = tk.Menu(self.window)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label='点我', menu=filemenu)
+        menubar.add_cascade(label='点我', menu=filemenu)
+        menubar.add_cascade(label='点我', menu=filemenu)
+        menubar.add_cascade(label='点我', menu=filemenu)        
+        def do_job():
+            pass
+        filemenu.add_command(label='★', command=do_job)
+        filemenu.add_command(label='邓', command=do_job)
+        filemenu.add_command(label='钰', command=do_job)
+        filemenu.add_command(label='可', command=do_job)
+        filemenu.add_command(label='太', command=do_job)
+        filemenu.add_command(label='帅', command=do_job)
+        filemenu.add_command(label='了', command=do_job)
+        filemenu.add_command(label='★', command=do_job)
+        # filemenu.add_separator()  
+        # filemenu.add_command(label='退出', command=self.window.quit)
+        self.window.config(menu=menubar)
 
 
     def login_face(self):
         face = tk.Frame(self.window)
-        face.pack()
-        # 第一行
-        l_username = tk.Label(face,text='用户名：')
-        l_username.grid(row=0)
-        username = tk.Entry(face)
-        username.grid(row=0,column=1)
-        # 2
-        l_password = tk.Label(face,text='密码：')
-        l_password.grid(row=1)
-        password = tk.Entry(face, show='*')
-        password.grid(row=1, column=1)
-        # 3
-        l_msg = tk.Label(face, text='')
-        l_msg.grid(row=4)
+        face.pack(expand='yes', fill="both")
+
+        tips = tk.StringVar()
+        tk.Label(face, text='账户:', font=('Arial', 14)).place(x=13, y=20)
+        tk.Label(face, text='密码:', font=('Arial', 14)).place(x=13, y=70)
+        tk.Label(face, textvariable=tips, fg='red').place(x=100, y=150)
+
+        var_usr_name = tk.StringVar()
+        entry_usr_name = tk.Entry(face, textvariable=var_usr_name, font=('Arial', 14))
+        entry_usr_name.place(x=120, y=20)
+        
+        var_usr_pwd = tk.StringVar()
+        entry_usr_pwd = tk.Entry(face, textvariable=var_usr_pwd, font=('Arial', 14), show='*')
+        entry_usr_pwd.place(x=120, y=70)
 
         def go_login():
-            phone_str = username.get()
-            password_str = password.get()
+            phone_str = entry_usr_name.get()
+            password_str = entry_usr_pwd.get()
             if not phone_str or not password_str:
-                l_msg['text'] = '请输入账户密码'
+                tips.set('请输入账户密码')
                 return 
                 
             data = {
-                "phone": username.get(),
-                "password": password.get()
+                "phone": entry_usr_name.get(),
+                "password": entry_usr_pwd.get()
             }
             headers = {
                 "Content-Type": "application/json"
@@ -54,56 +85,77 @@ class Talentback:
             req_obj = requests.post(self.base_url+"api/v1/accounts/login/", json=data, headers=headers)
             req_data = json.loads(req_obj.content)
             rc = req_obj.status_code            
-            print(str(rc), type(rc))
-            if rc == 404 or rc == 400:
-                l_msg['text'] = req_data.get("detail")
+            if str(rc).startswith('4'):
+                tips.set(req_data.get("detail"))
                 return
             else:
                 self.token = req_data.get('token')
-                face.forget()
+                face.destroy()
                 self.main_face()
                 logger.info(req_data)
 
         login_button = tk.Button(face, text='登录',  width=10, command=go_login)
         exit_button = tk.Button(face, text='退出',  width=10, command=self.window.destroy)
-        login_button.grid()
-        exit_button.grid()
+        login_button.place(x=100, y=110)
+        exit_button.place(x=180, y=110)
         # {
         # "phone": "15717974490",
         # "password": "123"
-        # } 
-        # 4
-        
-    def main_face(self):
-        face = tk.Frame(self.window)
-        face.pack()
-        button_list = self.get_method()
-        for this_bt in button_list:
-            this_button = tk.Button(face, text=this_bt,  width=10)
-            this_button.grid()        
-            
-        
+        # }
 
     def get_method(self):
-        # headers = {
-        #     "Content-Type": "application/json",
-        #     # "Authorier": self.token
-        # }
-        # req_obj = requests.get(self.base_url+"api/v1/oss/helper_method/", headers=headers)
-        # req_data = json.loads(req_obj.content)
-        # return [req_data]
-        # {'title': '注销', 'api/v1/asfasdf/', 
-        # 'canshu':{
-        #     "username": 'text',
-        #     "ss"
-        # }
-        # }
-        # post api/v1/asfasdf/  {
-        #     "username": username.get()
-        #     "ss": ss.get()
-        # }
-        return ['测试1', '测试2', '测试3','1','2','3','4','5','6','7','8','9','11','22','21','22','33','411']
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.token
+        }
+        req_obj = requests.get(self.base_url+"api/v1/helper/get-method/", headers=headers)
+        req_data = json.loads(req_obj.content)
+        # return req_data
+        return range(20)
 
+    def create_method(self, this_bt):
+        return print(1)
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.token
+        }
+        method = this_bt['method'].lower()
+        this_method = getattr(requests, method)
+        this_url = this_bt['url']
+        # lista = ["funca","funcb","funcc"]
+        
+        # for fn in lista:
+        #     exec(FUNC_TEMPLATE.format(func=fn))
+        # local_vars = dict(locals().items())
+        # funcs = [local_vars[f] for f in lista]
+        eval(FUNC_TEMPLATE.format())
+        rep_content = tk.Text(window, width=50, height=20)
+        this_method(this_url, headers=headers)
+
+    def main_face(self):        
+        method_frame = tk.LabelFrame(self.window, width=12, background='red')
+        method_frame.pack(side='left', fill='both')
+        button_list = self.get_method()
+        row = 0
+        for this_bt in button_list:
+            title = str(this_bt)
+            # title = this_bt['title']
+            # bt_method = self.create_method(this_bt)
+            # if not bt_method:
+            #     continue
+            this_button = tk.Button(method_frame, text=title[:5],  width=10, height=2)
+            this_button.grid(row=row, column=0)            
+            row += 1
+        
+        effect_frame = tk.LabelFrame(self.window, text="哦哦", background='yellow')
+        effect_frame.pack(side='right', padx=10, fill='both', expand='yes')
+
+
+FUNC_TEMPLATE = '''
+def go_{button}():
+    face = tk.Frame(self.window)
+    face.pack()
+'''
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
--- talentbacksolution.py
+++ talentbacksolution.py
@@ -8,89 +8,91 @@ import logging
 
 import tkinter as tk  # 使用Tkinter前需要先导入
  
-# # 第1步，实例化object，建立窗口window
-# window = tk.Tk()
+# 第1步，实例化object，建立窗口window
+window = tk.Tk()
  
-# # 第2步，给窗口的可视化起名字
-# window.title('达人推小帮手')
+# 第2步，给窗口的可视化起名字
+window.title('达人推小帮手')
  
-# # 第3步，设定窗口的大小(长 * 宽)
-# window.geometry('500x300')  # 这里的乘是小x
+# 第3步，设定窗口的大小(长 * 宽)
+window.geometry('500x300')  # 这里的乘是小x
  
-# # 第4步，在图形界面上设定输入框控件entry框并放置
-# e = tk.Entry(window,width=12, show = None)#显示成明文形式
-# e.place(x=10, y=15)
+# 第4步，在图形界面上设定输入框控件entry框并放置
+e = tk.Entry(window,width=12, show = None)#显示成明文形式
+e.place(x=10, y=15)
  
-# # 第5步，定义两个触发事件时的函数insert_point和insert_end（注意：因为Python的执行顺序是从上往下，所以函数一定要放在按钮的上面）
-# def insert_point(): # 在鼠标焦点处插入输入内容
-#     var = e.get()
-#     t.insert('insert', var)
-# def insert_end():   # 在文本框内容最后接着插入输入内容
-#     var = e.get()
-#     t.insert('end', var)
-# def insert_time():   # 在文本框内容最后接着插入输入内容
-    
-#     t.insert('end', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n' )
+# 第5步，定义两个触发事件时的函数insert_point和insert_end（注意：因为Python的执行顺序是从上往下，所以函数一定要放在按钮的上面）
+def insert_point(): # 在鼠标焦点处插入输入内容
+    var = e.get()
+    v.set(var)
+def insert_end():   # 在文本框内容最后接着插入输入内容
+    var = e.get()
+    v.set(var)
+def insert_time():   # 在文本框内容最后接着插入输入内容
+    var = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n'
+    v.set(var)
+    # t.insert('end', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n' )
  
-# # 第6步，创建并放置两个按钮分别触发两种情况
-# b1 = tk.Button(window, text='商家检查', width=10, height=2, command=insert_point)
-# b1.place(x=10, y=100)
-# b2 = tk.Button(window, text='达人检查', width=10,   height=2, command=insert_end)
-# b2.place(x=10, y=150)
-# b2 = tk.Button(window, text='关系修复', width=10,   height=2, command=insert_time)
-# b2.place(x=10, y=200)
+# 第6步，创建并放置两个按钮分别触发两种情况
+b1 = tk.Button(window, text='商家检查', width=10, height=2, command=insert_point)
+b1.place(x=10, y=100)
+b2 = tk.Button(window, text='达人检查', width=10,   height=2, command=insert_end)
+b2.place(x=10, y=150)
+b2 = tk.Button(window, text='关系修复', width=10,   height=2, command=insert_time)
+b2.place(x=10, y=200)
  
-# # 第7步，创建并放置一个多行文本框text用以显示，指定height=3为文本框是三个字符高度
-# t = tk.Text(window, width=50,height=20)
-# t.place(x=120, y=15)
+# 第7步，创建并放置一个多行文本框text用以显示，指定height=3为文本框是三个字符高度
+v = tk.StringVar
+t = tk.Label(window, textvariable=v, width=50,height=20)
+t.place(x=120, y=15)
  
-# # 第8步，主窗口循环显示
-# window.mainloop()
+# 第8步，主窗口循环显示
+window.mainloop()
 
 
-class TalentHelper:
-    def __init__(self):
+# class TalentHelper:
+#     def __init__(self):
         
-        self.window = tk.Tk()
-        self.base_url = 'http://api.etest.darentui.com/' # api/v1/home/statistic/
-        self.token = None
-        self.window.title('达人推小帮手')
-        self.window.geometry('500x300')      
-        self.login()
-        self.window.mainloop()
+#         self.window = tk.Tk()
+#         self.base_url = 'http://api.etest.darentui.com/' # api/v1/home/statistic/
+#         self.token = None
+#         self.window.title('达人推小帮手')
+#         self.window.geometry('500x300')      
+#         self.login()
+#         self.window.mainloop()
 
-    def send_post(self, url, data):        
-        headers = {
-            "Content-Type": "application/json"
-        }
-        req_obj = requests.post(url, data=json.dumps(data), headers=headers)
-        logger.info(json.loads(req_obj.content))
-        logger.info(json.dumps(data))
+#     def send_post(self, url, data):        
+#         headers = {
+#             "Content-Type": "application/json"
+#         }
+#         req_obj = requests.post(url, data=json.dumps(data), headers=headers)
+#         logger.info(json.loads(req_obj.content))
+#         logger.info(json.dumps(data))
 
-    def login(self):
-        url = self.base_url + 'api/v1/accounts/login/'
-        username = tk.Entry(self.window, width=20, show = None)
-        password = tk.Entry(self.window, width=20, show = None)
-        submit = tk.Button(self.window, text='登录确定', width=10, height=1)
-        username.place(x=244, y=60)
-        password.place(x=244, y=80)
-        submit.place(x=244, y=120)
+#     def login(self):
+#         url = self.base_url + 'api/v1/accounts/login/'
+#         username = tk.Entry(self.window, width=20, show = None)
+#         password = tk.Entry(self.window, width=20, show = None)
+#         submit = tk.Button(self.window, text='登录确定', width=10, height=1)
+#         username.place(x=244, y=60)
+#         password.place(x=244, y=80)
+#         submit.place(x=244, y=120)
 
-    def get_method(self):
-        pass
+#     def get_method(self):
+#         pass
 
-    def logout(self):
-        pass    
+#     def logout(self):
+#         pass    
 
 
 
-if __name__ == '__main__':    
-    # root = tk.Tk()
-    logger = logging.getLogger(__name__)
-    logger.setLevel(level = logging.INFO)
-    handler = logging.FileHandler("talentback_help_log.txt")
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    TalentHelper()
+# if __name__ == '__main__':    
+#     # root = tk.Tk()
+#     logger = logging.getLogger(__name__)
+#     logger.setLevel(level = logging.INFO)
+#     handler = logging.FileHandler("talentback_help_log.txt")
+#     handler.setLevel(logging.INFO)
+#     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     TalentHelper()
