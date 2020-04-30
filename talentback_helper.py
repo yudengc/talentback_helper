f88d9183d62f7c0753e9e09819e0af46c619db5b
dengyu <yudengc@outlook.com>
2020-04-30 18:24:48 +0800

    update

--- talentback_helper.py
+++ talentback_helper.py
@@ -1,10 +1,15 @@
 # -*- coding:utf-8 -*-
+# yudengc
 
+import base64
 import logging
 import json
 import requests
 import sys
 import tkinter as tk
+
+
+__VERSION__ = "1.0"
 
 class Talentback:
     def __init__(self):
@@ -19,12 +24,17 @@ class Talentback:
         self.window.geometry('{}x{}'.format(self.width, self.heigth))
         # self.window.resizable(width=False, height=False)
         self.window.minsize(400, 200)
-        self.window.maxsize(800, 400)
-        self.base_url = "http://api.etest.darentui.com/"        
+        self.window.maxsize(400, 800)
+        self.base_url = "http://api.etest.darentui.com/"
+        # self.base_url = "http://119.23.109.99:8083/"
+        self.update()
         self.menu()
-        # self.login_face()
-        self.main_face()
+        self.login_face()
+        # self.main_face()
         self.window.mainloop()
+
+    def update(self):
+        pass
 
     def menu(self):
         menubar = tk.Menu(self.window)
@@ -117,49 +127,117 @@ class Talentback:
         req_obj = requests.get(self.base_url+"api/v1/helper/get-wellcome/", headers=headers)
         return json.loads(req_obj.content)
 
-    def create_method(self, this_bt):
-        def go_0(self):
-            print(self)
-            this_frame = tk.LabelFrame(self.main_frame, text='操作')
+    def create_method(self, method_list):
+        func_template = '''def {func_name}(self):
+            this_frame = tk.LabelFrame(self.main_frame, text='注销')
             this_frame.pack(side='left', expand='yes', fill='both')
-            self.now_frame = this_frame
-            headers = {
-                "Content-Type": "application/json",
-                "Authorization": self.token
-            }
-            this_method = getattr(requests, 'get')
-            # this_method(self.base_url+'api/v1/helper/test/', headers=headers, data=json.dumps(data))
-            # args = json.loads({args})
-            args = [
-                {
-                    "arg_name": "username",
-                    "type": "Entry",
-                    "tips": "用户名",
-                }
-            ]
+            self.now_frame = this_frame                           
+            args = {args}
+            data = dict()
+            row = 0
+            column = 0
+            all_arg_widget = dict()
+            class follow_list(list):
+                def get(self):
+                    result = []
+                    for i in self.__iter__():
+                        result.append(i.get())
+                    return result
             for arg in args:
-                tk_type_name = arg['type']
-                tk_type = getattr(tk, tk_type_name)
-                v = tk.StringVar
-                entry_usr_name = tk.Entry(this_frame, textvariable=v, font=('Arial', 14))
-                entry_usr_name.pack()
-        setattr(self, 'go_0', go_0)
-        return
-        method = this_bt['method'].lower()
-        this_method = getattr(requests, method)
-        this_url = this_bt['url']       
-        eval(FUNC_TEMPLATE.format())
-        rep_content = tk.Text(window, width=50, height=20)
-        this_method(this_url, headers=headers)    
+                try:
+                    arg_name = arg['arg_name']
+                    tk_type_name = arg['type']
+                    tips = arg["tips"]
+                    if tk_type_name == '输入':
+                        tk.Label(this_frame, text=tips).grid(row=row, column=column, pady=2, padx=2)
+                        v = tk.StringVar
+                        arg_widget = tk.Entry(this_frame, textvariable=v)
+                        arg_widget.grid(row=row, column=column+1, pady=2, padx=2)                    
+                    elif tk_type_name == '单选':
+                        o = tk.LabelFrame(this_frame, text=tips)
+                        o.grid(row=row, column=column, pady=2, padx=2, columnspan=2, sticky=tk.EW)
+                        arg_widget = tk.IntVar()
+                        choice = arg['choice']
+                        length = len(choice)
+                        for i in range(length):
+                            tk.Radiobutton(o, text=choice[i], variable=arg_widget, value=i).grid(column=0, row=i, sticky=tk.W)
+                    elif tk_type_name == '多选':
+                        o = tk.LabelFrame(this_frame, text=tips)
+                        o.grid(row=row, column=column, pady=2, padx=2, columnspan=2, sticky=tk.EW)
+                        choice = arg['choice']
+                        length = len(choice)                        
+                        arg_widget = follow_list()
+                        for i in range(length):
+                            this_name = "var_%d" %i
+                            locals()[this_name] = tk.IntVar()
+                            this_var = locals()[this_name]
+                            arg_widget.append(this_var)
+                            tk.Checkbutton(o, text=choice[i], variable=this_var).grid(column=0, row=i, sticky=tk.W)                        
+                    else:
+                        continue
+                except Exception as e:
+                    print('err:')
+                    print(str(e))
+                    logger.info(str(e))
+                    continue
+                all_arg_widget[arg_name] = arg_widget
+                row += 1
+            rep_data = tk.StringVar()
+            var_usr_name = tk.StringVar()
+            self.rep_label = tk.Label(this_frame, textvariable=rep_data, wraplength=220, fg='green')
+            self.rep_label.grid(row=row, pady=5, rowspan=999, columnspan=999, sticky=tk.NSEW)
+            def send_request():
+                headers = dict()
+                headers["Content-Type"] = "application/json"
+                headers["Authorization"] = self.token           
+                data = dict()
+                for arg_name, arg_widget in all_arg_widget.items():
+                    data[arg_name] = arg_widget.get()
+                print(data)
+                get_method = '{method}'
+                get_url = '{url}'
+                this_method = getattr(requests, get_method)
+                rep = None
+                if get_method == 'post':
+                    rep = this_method(self.base_url+get_url, data=json.dumps(data), headers=headers)
+                elif get_method == 'get':
+                    rep = this_method(self.base_url+get_url, params=data, headers=headers)
+                response_data = "请求失败, 找一下技术哥哥看看吧"      
+                if not rep:
+                    self.rep_label.config(fg='red')
+                elif int(str(rep.status_code)[0]) > 4:
+                    self.rep_label.config(fg='red')
+                if rep is not None:                    
+                    response_data = json.loads(rep.content).get("detail", response_data)
+                rep_data.set(response_data)
+
+            exec_button = tk.Button(this_frame, text='执行', command=send_request)
+            exec_button.grid(row=0, column=3, pady=2, padx=5)'''
+
+        i = 0
+        for method in method_list:
+            func_name = 'go_{}'.format(i)
+            url = method['url']
+            req_method = method['method'].lower()
+            args = method['args']
+            func_str = func_template.format(func_name=func_name, url=url, method=req_method, args=args)
+            exec(func_str)
+            func = locals()[func_name]
+            setattr(self, func_name, func)
+            i += 1
+ 
 
     def main_face(self):
+        self.window.geometry('400x380')
         self.main_frame = tk.Frame(self.window)
         self.main_frame.pack(expand='yes', fill="both")
-        button_list = self.get_method()
+        method_list = self.get_method()
         wellcome_str = self.get_wellcome()
-        self.create_method(button_list)
+        self.create_method(method_list)
 
         def select_method(*args):
+            if not listbox.curselection():
+                return
             this_index = listbox.curselection()[0]
             if self.now_select == this_index:
                 return
@@ -179,7 +257,7 @@ class Talentback:
 
         listbox = tk.Listbox(
             left_frame, width=12, font=('Arial', 10), 
-            listvariable=tk.StringVar(value=[i['title'] for i in button_list]), 
+            listvariable=tk.StringVar(value=[i['title'] for i in method_list]), 
             selectmode="browse"
         )
         listbox.pack(side='left', fill='both')
@@ -191,19 +269,6 @@ class Talentback:
         y_scr.config(command=listbox.yview)
         y_scr.pack(side='right', fill='y')
 
-
-FUNC_TEMPLATE = '''
-def go_{index}(self):
-    this_frame = tk.LabelFrame(main_frame, text='操作')
-    this_frame.pack(side='left', expand='yes', fill='both')
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": self.token
-    }
-    this_method = getattr(requests, {method})
-    this_method(self.base_url+{url}, headers=headers, data=json.dumps(data))
-    args = json.loads({args})
-# '''
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
--- talentbacksolution.py
+++ talentbacksolution.py
@@ -1,98 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
+from tkinter import *
  
-import time
-import json
-import requests
-import logging
-
-import tkinter as tk  # 使用Tkinter前需要先导入
+# 初始化Tk()
+myWindow = Tk()
+# 设置标题
+myWindow.title('Python GUI Learning')
+myWindow.geometry("%dx%d+%d+%d"%(400, 200, 200, 200))
+# 创建Checkbutton
+checkVar = IntVar()
+check = Checkbutton(myWindow, text="Checkbutton test", variable=checkVar)
+check.grid(row=0, column=0, sticky=W, padx=2 ,pady=5)
+# 定义按钮点击事件
+def button_Click(event=None):
+    print(checkVar.get(), type(checkVar.get()))
  
-# 第1步，实例化object，建立窗口window
-window = tk.Tk()
+# 创建两个按钮
+b1 =Button(myWindow, text='click me' , relief='raised', width=8, height=1, command=button_Click)
+b1.grid(row=0, column=2, sticky=W, padx=2 ,pady=10)
  
-# 第2步，给窗口的可视化起名字
-window.title('达人推小帮手')
- 
-# 第3步，设定窗口的大小(长 * 宽)
-window.geometry('500x300')  # 这里的乘是小x
- 
-# 第4步，在图形界面上设定输入框控件entry框并放置
-e = tk.Entry(window,width=12, show = None)#显示成明文形式
-e.place(x=10, y=15)
- 
-# 第5步，定义两个触发事件时的函数insert_point和insert_end（注意：因为Python的执行顺序是从上往下，所以函数一定要放在按钮的上面）
-def insert_point(): # 在鼠标焦点处插入输入内容
-    var = e.get()
-    v.set(var)
-def insert_end():   # 在文本框内容最后接着插入输入内容
-    var = e.get()
-    v.set(var)
-def insert_time():   # 在文本框内容最后接着插入输入内容
-    var = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n'
-    v.set(var)
-    # t.insert('end', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n' )
- 
-# 第6步，创建并放置两个按钮分别触发两种情况
-b1 = tk.Button(window, text='商家检查', width=10, height=2, command=insert_point)
-b1.place(x=10, y=100)
-b2 = tk.Button(window, text='达人检查', width=10,   height=2, command=insert_end)
-b2.place(x=10, y=150)
-b2 = tk.Button(window, text='关系修复', width=10,   height=2, command=insert_time)
-b2.place(x=10, y=200)
- 
-# 第7步，创建并放置一个多行文本框text用以显示，指定height=3为文本框是三个字符高度
-v = tk.StringVar
-t = tk.Label(window, textvariable=v, width=50,height=20)
-t.place(x=120, y=15)
- 
-# 第8步，主窗口循环显示
-window.mainloop()
+# 进入消息循环
 
 
-# class TalentHelper:
-#     def __init__(self):
-        
-#         self.window = tk.Tk()
-#         self.base_url = 'http://api.etest.darentui.com/' # api/v1/home/statistic/
-#         self.token = None
-#         self.window.title('达人推小帮手')
-#         self.window.geometry('500x300')      
-#         self.login()
-#         self.window.mainloop()
+myWindow.mainloop()
 
-#     def send_post(self, url, data):        
-#         headers = {
-#             "Content-Type": "application/json"
-#         }
-#         req_obj = requests.post(url, data=json.dumps(data), headers=headers)
-#         logger.info(json.loads(req_obj.content))
-#         logger.info(json.dumps(data))
-
-#     def login(self):
-#         url = self.base_url + 'api/v1/accounts/login/'
-#         username = tk.Entry(self.window, width=20, show = None)
-#         password = tk.Entry(self.window, width=20, show = None)
-#         submit = tk.Button(self.window, text='登录确定', width=10, height=1)
-#         username.place(x=244, y=60)
-#         password.place(x=244, y=80)
-#         submit.place(x=244, y=120)
-
-#     def get_method(self):
-#         pass
-
-#     def logout(self):
-#         pass    
-
-
-
-# if __name__ == '__main__':    
-#     # root = tk.Tk()
-#     logger = logging.getLogger(__name__)
-#     logger.setLevel(level = logging.INFO)
-#     handler = logging.FileHandler("talentback_help_log.txt")
-#     handler.setLevel(logging.INFO)
-#     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#     handler.setFormatter(formatter)
-#     logger.addHandler(handler)
-#     TalentHelper()
